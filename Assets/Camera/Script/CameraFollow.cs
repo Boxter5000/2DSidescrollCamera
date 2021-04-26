38970c2ba4a51c7f5146465bf0af6b4749f90f4f
Boxter5000 <marc.schuster60@web.de>
2021-04-26 13:12:40 +0200

    Disgusting Foor Distening

--- Assets/Camera/Script/CameraFollow.cs
+++ Assets/Camera/Script/CameraFollow.cs
@@ -1,26 +1,47 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private GameObject player;
+    [SerializeField] private GameObject FloorDistance;
     [SerializeField] private float camerDistance;
     [SerializeField] private float Speed;
     [SerializeField]private float idleOffset = 1f;
-    
+    [SerializeField] private float maxFloorDistance;
+    [SerializeField] private LayerMask RayMask;
+    [SerializeField] private float FloormoveSpeed;
+
+    private float facing;
     private CharacterControllet _characterController;
     
     private void Awake()
     {
         _characterController = player.GetComponent<CharacterControllet>();
     }
-
+    
     public void FixedUpdate()
     {
-        Vector2 pos = Vector2.Lerp ((Vector2)transform.position, (Vector2)player.transform.position, Speed * Time.fixedDeltaTime);
+        Vector2 FloorDistancePos = new Vector2(FloorDistance.transform.position.x, FloorDistance.transform.position.y);
+        
+        RaycastHit2D hitDistanceDown = Physics2D.Raycast(FloorDistancePos, Vector2.down, maxFloorDistance, RayMask);
+        RaycastHit2D hitDistanceUp = Physics2D.Raycast(FloorDistancePos, Vector2.up, maxFloorDistance, RayMask);
+
+        float hitLenghtDown = hitDistanceDown.distance;
+        float hitLenghtUp = hitDistanceUp.distance;
+
+        Vector2 cameraMovePos =
+            new Vector2(player.transform.position.x + facing, player.transform.position.y + hitLenghtDown);
+
+        Vector2 pos = Vector2.Lerp(
+            (Vector2)transform.position,
+            (Vector2)player.transform.position,
+            Speed * Time.fixedDeltaTime);
+        
         if (_characterController.facingRight)
         {
             pos.x += idleOffset;
@@ -30,6 +51,14 @@ public class CameraFollow : MonoBehaviour
             pos.x += -idleOffset;
         }
 
+        if (hitDistanceDown)
+        {
+            pos.y += FloormoveSpeed;
+        }
+        if (hitDistanceUp)
+        {
+            pos.y -= FloormoveSpeed / 2;
+        }
         
         transform.position = new Vector3(pos.x, pos.y, camerDistance);
     }
